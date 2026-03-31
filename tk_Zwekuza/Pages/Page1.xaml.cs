6259bb312db92bb084fda27ffb92e1dede7e7c36
Анна Кузьмина <anna905k@icloud.com>
2026-03-31 14:55:12 +0300

    Закончили калькулятор и начали тесты

---
+++ tk_Zwekuza/Pages/Page1.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace tk_Zwekuza
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public static float otv;
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Plus_Checked(object sender, RoutedEventArgs e)
+        {
+            float.TryParse(x.Text, out float x1);
+            float.TryParse(y.Text, out float y1);
+
+            MathPlus(x1, y1);
+        }
+
+        private void Minus_Checked(object sender, RoutedEventArgs e)
+        {
+            float.TryParse(x.Text, out float x1);
+            float.TryParse(y.Text, out float y1);
+
+            MathMinus(x1, y1);
+        }
+
+        private void Ymn_Checked(object sender, RoutedEventArgs e)
+        {
+            float.TryParse(x.Text, out float x1);
+            float.TryParse(y.Text, out float y1);
+
+            MathYmn(x1, y1);
+        }
+
+        private void Del_Checked(object sender, RoutedEventArgs e)
+        {
+            float.TryParse(x.Text, out float x1);
+            float.TryParse(y.Text, out float y1);
+
+            MathDel(x1, y1);
+        }
+
+        public bool MathPlus(float x1, float y1)
+        {
+            otv = x1 + y1;
+            return true;
+        }
+        public bool MathMinus(float x1, float y1)
+        {
+            otv = x1 - y1;
+            return true;
+        }
+        public bool MathYmn(float x1, float y1)
+        {
+            otv = x1 * y1;
+            return true;
+        }
+        public bool MathDel(float x1, float y1)
+        {
+            if (y1 == 0)
+            {
+                MessageBox.Show("на 0 делить нельзя!");
+                Del.IsChecked = false;
+                return false;
+            }
+            else
+            {
+                otv = x1 / y1;
+                return true;
+            }
+        }
+
+        private void Rezult_Click(object sender, RoutedEventArgs e)
+        {
+            rez.Text = otv.ToString();
+        }
+
+        private void Clean_Click(object sender, RoutedEventArgs e)
+        {
+            x.Text = String.Empty;
+            y.Text = String.Empty;
+            rez.Text = String.Empty;
+        }
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            foreach (char c in e.Text)
+            {
+                if (!char.IsDigit(c))
+                {
+                    e.Handled = true;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ tk_Zwekuza/ZwekuzaTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using tk_Zwekuza;
+
+namespace ZwekuzaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodPlus()
+        {
+            var page = new Page1();
+            float x1 = 2;
+            float y1 = 2;
+            float rez = x1 + y1;
+            Assert.IsTrue(page.MathPlus(x1, y1), Convert.ToString(rez));
+        }
+
+        [TestMethod]
+        public void TestMethodMinus()
+        {
+            var page = new Page1();
+            float x1 = 2;
+            float y1 = 2;
+            float rez = x1 - y1;
+            Assert.IsTrue(page.MathMinus(x1, y1), Convert.ToString(rez));
+        }
+
+        [TestMethod]
+        public void TestMethodYmn()
+        {
+            var page = new Page1();
+            float x1 = 2;
+            float y1 = 2;
+            float rez = x1 * y1;
+            Assert.IsTrue(page.MathYmn(x1, y1), Convert.ToString(rez));
+        }
+
+        [TestMethod]
+        public void TestMethodDel()
+        {
+            var page = new Page1();
+            float x1 = 2;
+            float y1 = 2;
+            float rez = x1 / y1;
+            Assert.IsTrue(page.MathDel(x1, y1), Convert.ToString(rez));
+        }
+
+        [TestMethod]
+        public void TestMethodPlus1()
+        {
+            var page = new Page1();
+            float x1 = 9999999;
+            float y1 = 9999999;
+            float rez = x1 + y1;
+            Assert.IsTrue(page.MathPlus(x1, y1), Convert.ToString(rez));
+        }
+
+    }
+}
