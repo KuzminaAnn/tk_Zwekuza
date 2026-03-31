c658753b750d8660c6eb49715911b139ac2ddd55
Анна Кузьмина <anna905k@icloud.com>
2026-03-31 15:02:23 +0300

    Закончили написание тест-кейсов

--- tk_Zwekuza/ZwekuzaTest/UnitTest1.cs
+++ tk_Zwekuza/ZwekuzaTest/UnitTest1.cs
@@ -57,5 +57,54 @@ namespace ZwekuzaTest
             Assert.IsTrue(page.MathPlus(x1, y1), Convert.ToString(rez));
         }
 
+        [TestMethod]
+        public void TestMethodMinus1()
+        {
+            var page = new Page1();
+            float x1 = 9999999;
+            float y1 = 9999999;
+            float rez = x1 - y1;
+            Assert.IsTrue(page.MathMinus(x1, y1), Convert.ToString(rez));
+        }
+
+        [TestMethod]
+        public void TestMethodYmn1()
+        {
+            var page = new Page1();
+            float x1 = 9999999;
+            float y1 = 9999999;
+            float rez = x1 * y1;
+            Assert.IsTrue(page.MathYmn(x1, y1), Convert.ToString(rez));
+        }
+
+        [TestMethod]
+        public void TestMethodDel1()
+        {
+            var page = new Page1();
+            float x1 = 9999999;
+            float y1 = 9999999;
+            float rez = x1 / y1;
+            Assert.IsTrue(page.MathDel(x1, y1), Convert.ToString(rez));
+        }
+
+        [TestMethod]
+        public void TestMethodMinus2()
+        {
+            var page = new Page1();
+            float x1 = 9;
+            float y1 = 9999999;
+            float rez = x1 - y1;
+            Assert.IsTrue(page.MathMinus(x1, y1), Convert.ToString(rez));
+        }
+
+        [TestMethod]
+        public void TestMethodDel2()
+        {
+            var page = new Page1();
+            float x1 = 2;
+            float y1 = 0;
+            float rez = x1 / y1;
+            Assert.IsFalse(page.MathDel(x1, y1), Convert.ToString(rez));
+        }
     }
 }
